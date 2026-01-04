2565dbb501be6f962fedec6011acc0fd771c50cd
Matthias Scholz <matthias.scholz@thoughtworks.com>
2026-01-04 12:10:40 +0100

    ✨ demo led control

---
+++ demo_led.py
@@ -0,0 +1,23 @@
+# NOTE LED is 5V, but max voltage is 3.3 on the XIAO expension board
+#      Hence LED is very dimmed
+
+import time
+import board
+from digitalio import DigitalInOut, Direction
+
+# LED Pin
+pin = board.DO  # depends on the connector
+update_rate = 2  # in secs
+
+led = DigitalInOut(pin)
+led.direction = Direction.OUTPUT
+
+# TODO Toggle the LED blink by button press
+# TODO Display state of the LED on the display
+
+# Let the LED blink
+while True:
+    led.value = True
+    time.sleep(update_rate)
+    led.value = False
+    time.sleep(update_rate)
